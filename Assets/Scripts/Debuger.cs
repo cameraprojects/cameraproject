2df662850078182a70cc50e02357200d89060681
AYATYA2 <bf1@outlook.jp>
2023-06-06 17:26:12 +0900

    debegerの改良 develop用のシーン完成

--- Assets/Scripts/Debuger.cs
+++ Assets/Scripts/Debuger.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using Photon.Pun;
 public class Debuger : MonoBehaviourPun
 {
+        [SerializeField]
+    private float y_sensitivity = 3f;
+        [SerializeField]
+    private float x_sensitivity = 3f;
+
+        [SerializeField]
+    private float speed = 1f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,6 +29,39 @@ public class Debuger : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
-        
+        float y_mouse = Input.GetAxis("Mouse Y");
+        float x_mouse = Input.GetAxis("Mouse X");
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= y_mouse * y_sensitivity;
+        newRotation.y -= x_mouse * x_sensitivity;
+        transform.localEulerAngles = newRotation;
+        if(photonView.IsMine){
+            if(Input.GetKey(KeyCode.A)){
+                this.transform.position+=speed*-this.transform.right;
+            }
+            if(Input.GetKey(KeyCode.W)){
+                this.transform.position+=speed*this.transform.forward;
+            }
+            if(Input.GetKey(KeyCode.S)){
+                this.transform.position+=speed*-this.transform.forward; 
+            }
+            if(Input.GetKey(KeyCode.D)){
+                this.transform.position+=speed*this.transform.right;
+            }
+            if(Input.GetKeyDown(KeyCode.T)){
+                if(this.tag=="Player"){
+                    this.tag="Untagged";
+                }else{
+                    this.tag="Player";
+                }
+            }
+            if(Input.GetKey(KeyCode.Q)){
+                this.transform.position+=speed*this.transform.up;
+            }
+            if(Input.GetKey(KeyCode.Z)){
+                this.transform.position+=speed*-this.transform.up;
+            }
+            
+        }
     }
 }
