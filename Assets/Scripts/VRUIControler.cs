88970acde9e6fbeddd3b7e45102764e8b9b4a452
AYATYA2 <bf1@outlook.jp>
2023-06-22 15:44:03 +0900

    撮った写真を見せるGUI改良　スクロールバーを作成

--- Assets/Scripts/VRUIControler.cs
+++ Assets/Scripts/VRUIControler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using Unity.XR.CoreUtils;
 using UnityEngine.InputSystem;
@@ -14,20 +15,29 @@ public class VRUIControler : MonoBehaviour
     public GameObject vrui; 
     private string filePath;
     private string[] _fileinfo;
-    public GameObject Pictures; 
-    public int c=0;   
+    public GameObject Pictures;
+    //public int c=0;   
     void Start()
     {
         LeftButton.action.started += UIOpen;
         filePath = Application.dataPath + "/Pictures/CameraScreenShot/";
+        vrui.transform.Find("Scrollbar").GetComponent<Scrollbar>().onValueChanged.AddListener(scrollbarmoved);
     }
-
+    void scrollbarmoved(float val){
+         int c=vrui.transform.childCount;
+            for(int i=1;i<c;i++){
+                if(vrui.transform.GetChild(i)){
+                    vrui.transform.GetChild(i).localPosition=new Vector3(vrui.transform.GetChild(i).localPosition.x,-(float)(i/5)*15f+vrui.transform.Find("Scrollbar").GetComponent<Scrollbar>().value*(float)(vrui.transform.Find("Scrollbar").GetComponent<Scrollbar>().numberOfSteps),vrui.transform.GetChild(i).localPosition.z);
+                }
+            }
+    }
+    
     void UIOpen(InputAction.CallbackContext context)
     {
         if (vrui.activeSelf)
         {
-            c=vrui.transform.childCount;
-            for(int i=0;i<c;i++){
+            int c=vrui.transform.childCount;
+            for(int i=1;i<c;i++){
                 if(vrui.transform.GetChild(i)){
                     Destroy(vrui.transform.GetChild(i).gameObject);
                 }
@@ -42,15 +52,19 @@ public class VRUIControler : MonoBehaviour
     }
 
     void UIUpdate(){
-         foreach (string file in Directory.GetFiles(filePath,"*.png"))
+        int i=0; 
+        foreach (string file in Directory.GetFiles(filePath,"*.png"))
         {
+            Vector3 photopos=new Vector3((float)(i%5-2)*0.1f,-(float)(i/5)*0.1f,0);
             byte[] bytes = File.ReadAllBytes(file);
-            Texture2D texture = new Texture2D(10, 10);
+            Texture2D texture = new Texture2D(1, 1);
 		    texture.filterMode = FilterMode.Trilinear;
 		    texture.LoadImage(bytes);
-            var newbutton = Instantiate(Pictures,this.transform.position,this.transform.rotation,vrui.transform);
+            var newbutton = Instantiate(Pictures,this.transform.position+photopos,this.transform.rotation,vrui.transform);
             newbutton.GetComponent<Image>().sprite = Sprite.Create(texture,new Rect(0,0, texture.width,texture.height),Vector2.zero) ;
-            
+            i++;
         }
+        
+        vrui.transform.Find("Scrollbar").GetComponent<Scrollbar>().numberOfSteps=(i/5)+1;
     }
 }
